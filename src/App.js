7c4408ba83084c24f25e91aac6ea56e591c4c4c6
Jo Bos <jobos@Jos-MacBook-Pro.local>
2022-06-27 15:19:20 +0200

    Nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+//Nav
+import Nav from './components/Nav'
+
+
+//Pages
 import AboutUs from './pages/AboutUs';
+
+//Global Style
 import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
      
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1><a id="logo" href="#">Creative Web Vibes</a></h1>
+      <ul>
+        <li>
+          <a href="#">1. About Us</a>
+        </li>
+        <li>
+          <a href="#">2. Our Work</a>
+        </li>
+        <li>
+          <a href="#">1. Contact Us</a>
+        </li>
+      </ul>
+
+    </StyledNav>
+  )
+}
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #282828;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+
+  }
+
+  #logo {
+    font-size: 1.6rem;
+    font-family: 'Lobster', cursive;
+    font-weight: lighter;
+  }
+
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+`;
+
+export default Nav;
