005418f43a71d81449e6e335284c2c81a5b84b8d
Jo Bos <jobos@Jos-MacBook-Pro.local>
2022-06-27 14:26:41 +0200

    Intro To Styled componnets

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServicesSection';
-import Faq from '../components/FaqSection';
 import FaqSection from '../components/FaqSection';
 
 
