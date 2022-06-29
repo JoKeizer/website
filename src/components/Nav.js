5bc1a70d285fc751361546c4c9492318ade36bd1
Jo Bos <jobos@Jos-MacBook-Pro.local>
2022-06-29 22:56:37 +0200

    added animated nav line

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,49 @@
 import React from 'react';
-
 import styled from 'styled-components';
+import { Link, useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Nav = () => {
+  const location = useLocation();
+  console.log(location.pathname)  
+
+  const activePath = location.pathname;
   return (
     <StyledNav>
-      <h1><a id="logo" href="/">Creative Web Vibes</a></h1>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
       <ul>
         <li>
-          <a href="/">1. About Us</a>
+          <Link to="/">1. About us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: activePath === '/' ? '50%' : '0%' }}
+          />
         </li>
         <li>
-          <a href="/work">2. Our Work</a>
+          <Link to="/work">2. Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: activePath === '/work' ? '50%' : '0%' }}
+          />
         </li>
         <li>
-          <a href="/contact">1. Contact Us</a>
+          <Link to="/contact">1. Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: activePath === '/contact' ? '50%' : '0%' }}
+          />
         </li>
       </ul>
-
     </StyledNav>
-  )
-}
+  );
+};
 
 const StyledNav = styled.nav`
   min-height: 10vh;
@@ -28,51 +51,52 @@ const StyledNav = styled.nav`
   margin: auto;
   justify-content: space-between;
   align-items: center;
+  background: #282828;
   padding: 1rem 10rem;
-  background-color: #282828;
   position: sticky;
   top: 0;
   z-index: 10;
 
-  a {
-    color: white;
-    text-decoration: none;
-  }
-
   ul {
-    list-style: none;
     display: flex;
-
+    list-style: none;
   }
-
-  #logo {
-    font-size: 1.6rem;
-    font-family: 'Lobster', cursive;
-    font-weight: lighter;
-  }
-
   li {
     padding-left: 10rem;
     position: relative;
   }
-
-  @media (max-width: 1300px) {
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: 'Lobster', cursive;
+    font-weight: lighter;
+  }
+  @media (max-width: 1500px) {
     flex-direction: column;
-    padding: 5rem1rem 1rem 1rem;
-
-    #logo {
-      display: inline-block;
-      margin: 2rem;
-    }
+    padding: 2rem 0rem;
     ul {
       padding: 2rem;
       justify-content: space-around;
       width: 100%;
-
       li {
         padding: 0;
       }
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 5%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1500px) {
+    left: 0%;
   }
 `;
 
